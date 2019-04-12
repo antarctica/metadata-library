33166287312c32d3be035e5d7e94e91f28ebfff6
Felix Fennell <felix@felixfennell.co.uk>
2019-04-12 09:39:25 +0100

    Adding XML declaration tests
    Closes #3.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,14 @@
 import unittest
 
+from http import HTTPStatus
+
 from flask import current_app
+
+# Exempting Bandit security issue (Using Element to parse untrusted XML data is known to be vulnerable to XML attacks)
+#
+# This is a testing environment, testing against endpoints that don't themselves allow user input, so the XML returned
+# should be safe. In any case the test environment is not exposed and so does not present a risk.
+from lxml import etree  # nosec
 
 from uk_pdc_metadata_record_generator import create_app
 
@@ -27,3 +35,16 @@ class AppTestCase(BaseTestCase):
 
     def test_app_is_testing(self):
         self.assertTrue(current_app.config['TESTING'])
+
+    def test_record_xml_declaration(self):
+        response = self.client.get(
+            '/',
+            base_url='http://localhost:9000'
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.mimetype, 'text/xml')
+
+        response_xml = etree.ElementTree(etree.XML(response.data))
+        self.assertEqual(response_xml.docinfo.xml_version, '1.0')
+        self.assertEqual(response_xml.docinfo.encoding, 'utf-8')
