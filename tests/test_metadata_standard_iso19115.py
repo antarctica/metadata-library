5cd1c05c3b27ae7eaefd73c7bb6aec7a3bbaa7ed
Felix Fennell <felix@felixfennell.co.uk>
2019-06-25 09:25:31 +0100

    Adding whole record tests
    To verify records are produced exactly as we intend we now compare against a rendered set of records.

--- tests/test_metadata_standard_iso19115.py
+++ tests/test_metadata_standard_iso19115.py
@@ -367,6 +367,12 @@ class MinimalMetadataRecordTestCase(BaseTestCase):
         self.assertDictEqual(self.test_response.nsmap, self.ns.nsmap())
         self.assertEqual(self.test_response.attrib[f"{{{self.ns.xsi}}}schemaLocation"], self.ns.schema_locations())
 
+    def test_record_contents(self):
+        with open(f"tests/resources/records/iso-19115-v1/{self.configuration}-record.xml") as expected_contents_file:
+            expected_contents = expected_contents_file.read()
+            # noinspection PyUnresolvedReferences
+            self.assertEqual(expected_contents, self.test_document.decode())
+
     # Element tests
 
     def test_record_file_identifier(self):
