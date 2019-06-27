b33680f3420b99952b2d6e34738061c5547d362a
Felix Fennell <felix@felixfennell.co.uk>
2019-06-27 22:10:34 +0100

    Adding python packaging tools and configuration

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = '0.1.0'
+
+# If a tagged commit, don't make a pre-release
+if 'CI_COMMIT_TAG' not in os.environ:
+    version = f"{ version }.dev{ os.getenv('CI_PIPELINE_ID') or None }"
+
+setup(
+    name="bas-metadata-library",
+    version=version,
+    author="British Antarctic Survey",
+    author_email="webapps@bas.ac.uk",
+    description="Python library for generating metadata records",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/antarctica/metadata-library",
+    license='Open Government Licence v3.0',
+    install_requires=['jsonschema', 'lxml', 'rfc3987', 'strict-rfc3339'],
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Software Development :: Libraries"
+    ],
+)
