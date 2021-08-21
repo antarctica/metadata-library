db1f58b5611885e03a86c8a3e39a077fac193249
Felix Fennell <felnne@bas.ac.uk>
2021-08-21 18:33:14 +0100

    Properly removing data quality measures support

--- bas_metadata_library/standards/iso_19115_common/data_quality_elements.py
+++ bas_metadata_library/standards/iso_19115_common/data_quality_elements.py
@@ -12,15 +12,6 @@ class DataQuality(MetadataRecordElement):
     def make_config(self) -> dict:
         _ = {}
 
-        report = Report(
-            record=self.record,
-            attributes=self.attributes,
-            xpath=f"{self.xpath}/gmd:dataQualityInfo/gmd:DQ_DataQuality/gmd:report/",
-        )
-        _report = report.make_config()
-        if bool(_report):
-            _["measures"] = [_report]
-
         lineage = Lineage(
             record=self.record,
             attributes=self.attributes,
@@ -39,16 +30,6 @@ class DataQuality(MetadataRecordElement):
         scope = Scope(record=self.record, attributes=self.attributes, parent_element=data_quality_element)
         scope.make_element()
 
-        if "measures" in self.attributes["resource"]:
-            for measure_attributes in self.attributes["resource"]["measures"]:
-                report = Report(
-                    record=self.record,
-                    attributes=self.attributes,
-                    parent_element=data_quality_element,
-                    element_attributes=measure_attributes,
-                )
-                report.make_element()
-
         lineage = Lineage(
             record=self.record,
             attributes=self.attributes,
@@ -65,90 +46,6 @@ class Scope(MetadataRecordElement):
 
         scope_code = ScopeCode(record=self.record, attributes=self.attributes, parent_element=scope_element)
         scope_code.make_element()
-
-
-class Report(MetadataRecordElement):
-    def make_config(self) -> dict:
-        _ = {}
-
-        report_code = self.record.xpath(
-            f"{self.xpath}/gmd:DQ_DomainConsistency/gmd:measureIdentification/gmd:RS_Identifier/gmd:code/"
-            f"gco:CharacterString/text()",
-            namespaces=self.ns.nsmap(),
-        )
-        if len(report_code) == 1:
-            _["code"] = report_code[0]
-
-        report_code_space = self.record.xpath(
-            f"{self.xpath}/gmd:DQ_DomainConsistency/gmd:measureIdentification/gmd:RS_Identifier/gmd:codeSpace/"
-            f"gco:CharacterString/text()",
-            namespaces=self.ns.nsmap(),
-        )
-        if len(report_code_space) == 1:
-            _["code_space"] = report_code_space[0]
-
-        specification = Citation(
-            record=self.record,
-            attributes=self.attributes,
-            xpath=f"{self.xpath}/gmd:DQ_DomainConsistency/gmd:result/gmd:DQ_ConformanceResult/gmd:specification",
-        )
-        _specification = specification.make_config()
-        if bool(_specification):
-            _ = {**_, **_specification}
-
-        report_explanation = self.record.xpath(
-            f"{self.xpath}/gmd:DQ_DomainConsistency/gmd:result/gmd:DQ_ConformanceResult/gmd:explanation/"
-            f"gco:CharacterString/text()",
-            namespaces=self.ns.nsmap(),
-        )
-        if len(report_explanation) == 1:
-            _["explanation"] = report_explanation[0]
-
-        report_result = self.record.xpath(
-            f"{self.xpath}/gmd:DQ_DomainConsistency/gmd:result/gmd:DQ_ConformanceResult/gmd:pass/gco:Boolean/text()",
-            namespaces=self.ns.nsmap(),
-        )
-        if len(report_result) == 1:
-            _["pass"] = bool(report_result[0])
-
-        return _
-
-    def make_element(self):
-        report_wrapper = SubElement(self.parent_element, f"{{{self.ns.gmd}}}report")
-        report_element = SubElement(report_wrapper, f"{{{self.ns.gmd}}}DQ_DomainConsistency")
-
-        identification_wrapper = SubElement(report_element, f"{{{self.ns.gmd}}}measureIdentification")
-        identification_element = SubElement(identification_wrapper, f"{{{self.ns.gmd}}}RS_Identifier")
-
-        identification_code_element = SubElement(identification_element, f"{{{self.ns.gmd}}}code")
-        identification_code_value = SubElement(identification_code_element, f"{{{self.ns.gco}}}CharacterString")
-        identification_code_value.text = self.element_attributes["code"]
-
-        identification_code_space_element = SubElement(identification_element, f"{{{self.ns.gmd}}}codeSpace")
-        identification_code_space_value = SubElement(
-            identification_code_space_element, f"{{{self.ns.gco}}}CharacterString"
-        )
-        identification_code_space_value.text = self.element_attributes["code_space"]
-
-        result_wrapper = SubElement(report_element, f"{{{self.ns.gmd}}}result")
-        result_element = SubElement(result_wrapper, f"{{{self.ns.gmd}}}DQ_ConformanceResult")
-
-        specification_element = SubElement(result_element, f"{{{self.ns.gmd}}}specification")
-        citation = Citation(
-            record=self.record,
-            attributes=self.attributes,
-            parent_element=specification_element,
-            element_attributes=self.element_attributes,
-        )
-        citation.make_element()
-
-        explanation_element = SubElement(result_element, f"{{{self.ns.gmd}}}explanation")
-        explanation_value = SubElement(explanation_element, f"{{{self.ns.gco}}}CharacterString")
-        explanation_value.text = self.element_attributes["explanation"]
-
-        pass_element = SubElement(result_element, f"{{{self.ns.gmd}}}pass")
-        pass_value = SubElement(pass_element, f"{{{self.ns.gco}}}Boolean")
-        pass_value.text = str(self.element_attributes["pass"]).lower()
 
 
 class Lineage(MetadataRecordElement):
