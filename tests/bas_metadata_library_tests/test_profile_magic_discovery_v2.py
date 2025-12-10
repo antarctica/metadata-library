6a4d21e1c3e9ca60bdb6cbfe893c73964d213978
Felix Fennell <felnne@bas.ac.uk>
2025-12-10 19:54:37 +0000

    Improving discovery profile tests

--- tests/bas_metadata_library_tests/test_profile_magic_discovery_v2.py
+++ tests/bas_metadata_library_tests/test_profile_magic_discovery_v2.py
@@ -7,7 +7,6 @@ from flask.testing import FlaskClient
 from jsonschema.exceptions import ValidationError
 from lxml.etree import tostring, Element
 
-from bas_metadata_library import RecordValidationError
 from bas_metadata_library.standards.iso_19115_2 import (
     MetadataRecord,
     MetadataRecordConfigV4,
@@ -21,12 +20,19 @@ namespaces = Namespaces()
 
 @pytest.mark.parametrize("config_name", list(configs_v2_all.keys()))
 def test_config_schema_validation_valid(config_name: str):
+    """Can validate minimal record configs against schema."""
     config = MetadataRecordConfigV4(**configs_v2_all[config_name])
     config.validate()
     assert True is True
 
 
 def test_config_schema_validation_invalid_profile():
+    """
+    Cannot validate record configs that don't match schema.
+
+    This is a basic test, to check the schema is loaded and the main, mandatory, parts of the schema work as expected.
+    Conditional parts of the schema are tested elsewhere.
+    """
     config = deepcopy(MetadataRecordConfigV4(**configs_v2_all["minimal_product_v2"]))
     del config.config["file_identifier"]
     with pytest.raises(ValidationError) as e:
@@ -36,6 +42,7 @@ def test_config_schema_validation_invalid_profile():
 
 @pytest.mark.parametrize("config_name", list(configs_v2_all.keys()))
 def test_response(app_client: FlaskClient, config_name: str):
+    """Can encode records for schema configs."""
     response = app_client.get(f"/profiles/{profile}/{config_name}")
     assert response.status_code == HTTPStatus.OK
     assert response.mimetype == "text/xml"
@@ -43,6 +50,7 @@ def test_response(app_client: FlaskClient, config_name: str):
 
 @pytest.mark.parametrize("config_name", list(configs_v2_all.keys()))
 def test_complete_record(app_client: FlaskClient, config_name: str):
+    """Check encoded records match known good examples."""
     with Path().resolve().parent.joinpath(f"resources/records/{profile}/{config_name}-record.xml").open() as expected_contents_file:
         expected_contents = expected_contents_file.read()
 
@@ -52,6 +60,7 @@ def test_complete_record(app_client: FlaskClient, config_name: str):
 
 @pytest.mark.parametrize("config_name", list(configs_v2_all.keys()))
 def test_parse_existing_record(config_name: str):
+    """Can decode a record to a record config."""
     with Path().resolve().parent.joinpath(f"resources/records/{profile}/{config_name}-record.xml").open() as record_file:
         record_data = record_file.read()
 
@@ -63,6 +72,7 @@ def test_parse_existing_record(config_name: str):
 
 @pytest.mark.parametrize("config_name", list(configs_v2_all.keys()))
 def test_lossless_conversion(fx_get_record_response: Element, config_name: str):
+    """Can encode then decode a record config without loss of information."""
     _record = tostring(
         fx_get_record_response(kind="profiles", standard_profile=profile, config=config_name),
         pretty_print=True,
